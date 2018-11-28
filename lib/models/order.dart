dd3826814ec5122714201dfb1596b56c8ee8db3b
trodrigu <trodriguez91@icloud.com>
2018-11-27 20:12:27 -0800

    rm underscore in order model

--- lib/models/order.dart
+++ lib/models/order.dart
@@ -1,33 +1,33 @@
 class Order {
-  String _dogName;
-  String _phoneNumber;
-  String _shippingAddress;
-  String _contactNumber;
-  String _wood;
-  String _design;
-  String _size;
+  String dogName;
+  String phoneNumber;
+  String shippingAddress;
+  String contactNumber;
+  String wood;
+  String design;
+  String size;
 
-  Order(this._dogName, this._phoneNumber, this._shippingAddress, this._contactNumber, this._wood, this._design, this._size);
+  Order(this.dogName, this.phoneNumber, this.shippingAddress, this.contactNumber, this.wood, this.design, this.size);
 
   Order.map(dynamic obj) {
-    this._dogName = obj["dog_name"];
-    this._phoneNumber = obj["phone_number"];
-    this._shippingAddress = obj["shipping_address"];
-    this._contactNumber = obj["contact_number"];
-    this._wood = obj["wood"];
-    this._design = obj["design"];
-    this._size = obj["size"];
+    this.dogName = obj["dog_name"];
+    this.phoneNumber = obj["phone_number"];
+    this.shippingAddress = obj["shipping_address"];
+    this.contactNumber = obj["contact_number"];
+    this.wood = obj["wood"];
+    this.design = obj["design"];
+    this.size = obj["size"];
   }
 
   Map<String, dynamic> toMap() {
     var map = new Map<String, dynamic>();
-    map["dogName"] = _dogName;
-    map["phoneNumber"] = _phoneNumber;
-    map["shippingAddress"] = _shippingAddress;
-    map["contactNumber"] = _contactNumber;
-    map["wood"] = _wood;
-    map["design"] = _design;
-    map["size"] = _size;
+    map["dogName"] = dogName;
+    map["phoneNumber"] = phoneNumber;
+    map["shippingAddress"] = shippingAddress;
+    map["contactNumber"] = contactNumber;
+    map["wood"] = wood;
+    map["design"] = design;
+    map["size"] = size;
 
     return map;
   }
